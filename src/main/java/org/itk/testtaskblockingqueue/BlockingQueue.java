9f6b0c1fd7b334e6df0d27cf911ae971bb91a552
Canib-us <162473942+Canib-us@users.noreply.github.com>
2025-08-03 16:23:46 +0700

    Update BlockingQueue.java

--- src/main/java/org/itk/testtaskblockingqueue/BlockingQueue.java
+++ src/main/java/org/itk/testtaskblockingqueue/BlockingQueue.java
@@ -1,4 +1,47 @@
 package org.itk.testtaskblockingqueue;
 
-public class BlockingQueue {
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class BlockingQueue <T>{
+    private final Queue<T> queue;
+    private final int capacity;
+    private final Object lock = new Object();
+
+    public BlockingQueue(int capacity) {
+        if(capacity <= 0){
+            throw new IllegalArgumentException();
+        }
+        this.queue = new LinkedList<T>();
+        this.capacity = capacity;
+    }
+
+    public void enqueue(T t) throws InterruptedException {
+        synchronized (lock) {
+            while (queue.size() >= capacity) {
+                lock.wait();
+            }
+            queue.offer(t);
+            System.out.println("Enqueued " + t+", size="+queue.size());
+            lock.notifyAll();
+        }
+    }
+
+    public T dequeue() throws InterruptedException {
+        synchronized (lock) {
+            while (queue.isEmpty()) {
+                lock.wait();
+            }
+            T t = queue.poll();
+            System.out.println("Dequeued " + t + ", size=" + queue.size());
+            lock.notifyAll();
+            return t;
+        }
+    }
+
+    public int size() {
+        synchronized (lock) {
+            return queue.size();
+        }
+    }
 }
