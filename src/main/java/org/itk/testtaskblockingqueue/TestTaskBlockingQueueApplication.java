e5e50f0538f2a42001703643c759e1e53d557419
Canib-us <162473942+Canib-us@users.noreply.github.com>
2025-08-03 16:24:35 +0700

    Update TestTaskBlockingQueueApplication.java

--- src/main/java/org/itk/testtaskblockingqueue/TestTaskBlockingQueueApplication.java
+++ src/main/java/org/itk/testtaskblockingqueue/TestTaskBlockingQueueApplication.java
@@ -8,6 +8,69 @@ public class TestTaskBlockingQueueApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(TestTaskBlockingQueueApplication.class, args);
+
+        BlockingQueue<String> queue = new BlockingQueue<>(5);
+        Thread producerThread = new Thread(() -> {
+            try {
+                for (int i = 0; i < 5; i++) {
+                    queue.enqueue("task 1 " + i);
+                    Thread.sleep(1000);
+                }
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+        }, "producerThread");
+
+        Thread producerThread1 = new Thread(() -> {
+            try {
+                for (int i = 0; i < 3; i++) {
+                    queue.enqueue("task 2 " + i);
+                    Thread.sleep(1000);
+                }
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+        }, "producerThread1");
+
+        Thread consumerThread = new Thread(() -> {
+            try {
+                for (int i = 0; i < 5; i++) {
+                    String task = queue.dequeue();
+                    System.out.println("consumerThread received "+task);
+                    Thread.sleep(2000);
+                }
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+        }, "consumerThread");
+
+        Thread consumerThread1 = new Thread(() -> {
+            try {
+                for (int i = 0; i < 3; i++) {
+                    String task = queue.dequeue();
+                    System.out.println("consumerThread1 received "+task);
+                    Thread.sleep(3000);
+                }
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+        }, "consumerThread1");
+
+        producerThread.start();
+        producerThread1.start();
+        consumerThread.start();
+        consumerThread1.start();
+
+        try {
+            producerThread.join();
+            producerThread1.join();
+            consumerThread.join();
+            consumerThread1.join();
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+
+        System.out.println("main end, final size is "+queue.size());
     }
 
 }
